f3dcd46f3ca1ac8ee3c6da5eac8ae166c188ca48
sergeyveneckiy <sergey.veneckiy@mail.ru>
2019-01-30 15:36:32 +0300

    Edited build.sh

--- HeedBook/build.sh
+++ HeedBook/build.sh
@@ -2,6 +2,8 @@ docker build -t heedbookregistry.azurecr.io/heedbookdev/faceanalyzeservice:lates
 docker build -t heedbookregistry.azurecr.io/heedbookdev/operationservice:latest -f OperationService.Dockerfile . &&
 docker build -t heedbookregistry.azurecr.io/heedbookdev/userservice:latest -f UserService.Dockerfile . && 
 docker build -t heedbookregistry.azurecr.io/heedbookdev/framefromvideoservice:latest -f FrameFromVideoService.Dockerfile . && 
+cd ExtractFramesFromVideo &&
+docker build -t heedbookregistry.azurecr.io/heedbookdev/extractfromframesservice:latest . &&
 
 docker push heedbookregistry.azurecr.io/heedbookdev/cognitiveservice:latest &&
 docker push heedbookregistry.azurecr.io/heedbookdev/operationservice:latest &&
