1cd0881ebc76eb1556db57a40fe8dd0522dd6a9c
Khamitov Daniyar <khamitov.dan@gmail.com>
2019-01-31 23:32:26 +0300

    added docker file for filling frame service

--- HeedBook/build.sh
+++ HeedBook/build.sh
@@ -2,10 +2,13 @@ docker build -t heedbookregistry.azurecr.io/heedbookdev/faceanalyzeservice:lates
 docker build -t heedbookregistry.azurecr.io/heedbookdev/operationservice:latest -f OperationService.Dockerfile . &&
 docker build -t heedbookregistry.azurecr.io/heedbookdev/userservice:latest -f UserService.Dockerfile . && 
 docker build -t heedbookregistry.azurecr.io/heedbookdev/framefromvideoservice:latest -f FrameFromVideoService.Dockerfile . && 
+docker build -t heedbookregistry.azurecr.io/heedbookdev/fillingframeservice:latest -f FillingFrameService.Dockerfile . &&
 cd ExtractFramesFromVideo &&
 docker build -t heedbookregistry.azurecr.io/heedbookdev/extractfromframesservice:latest . &&
 
 docker push heedbookregistry.azurecr.io/heedbookdev/cognitiveservice:latest &&
 docker push heedbookregistry.azurecr.io/heedbookdev/operationservice:latest &&
 docker push heedbookregistry.azurecr.io/heedbookdev/userservice:latest && 
-docker push heedbookregistry.azurecr.io/heedbookdev/framefromvideoservice:latest
+docker push heedbookregistry.azurecr.io/heedbookdev/framefromvideoservice:latest &&
+docker push heedbookregistry.azurecr.io/heedbookdev/extractfromframesservice:latest && 
+docker push heedbookregistry.azurecr.io/heedbookdev/fillingframeservice:latest
