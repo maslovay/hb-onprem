8eb219da1400117a5c42e67965a736faa708179a
Khamitov Daniyar <khamitov@heedbook.com>
2019-03-19 17:24:39 +0300

    added deleting old files

--- HeedBook/QuartzExtensions/ConfigureQuartz.cs
+++ HeedBook/QuartzExtensions/ConfigureQuartz.cs
@@ -59,5 +59,31 @@ namespace QuartzExtensions
                 return scheduler;
             });
         }
+
+        public static void AddDeleteOldFilesQuartz(this IServiceCollection services)
+        {
+            services.Add(new ServiceDescriptor(typeof(IJob), typeof(DeleteOldFilesJob), ServiceLifetime.Singleton));
+            services.AddSingleton<IJobFactory, ScheduledJobFactory>();
+            services.AddSingleton(provider => JobBuilder.Create<DialogueStatusCheckerJob>()
+                                                        .WithIdentity("DeleteOldFiles.job", "Files")
+                                                        .Build());
+            services.AddSingleton(provider =>
+            {
+                return TriggerBuilder.Create()
+                                     .WithIdentity($"DeleteOldFiles.trigger", "Files")
+                                     .StartNow()
+                                     .WithSimpleSchedule(s => s.WithIntervalInHours(1).RepeatForever())
+                                     .Build();
+            });
+
+            services.AddSingleton(provider =>
+            {
+                var schedulerFactory = new StdSchedulerFactory();
+                var scheduler = schedulerFactory.GetScheduler().Result;
+                scheduler.JobFactory = provider.GetService<IJobFactory>();
+                scheduler.Start();
+                return scheduler;
+            });
+        }
     }
 }
