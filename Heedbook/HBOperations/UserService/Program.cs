0d778e3b4f305d4619bbab6b3643abf22364439c
devgopher <evdokimov@heedbook.com>
2019-04-19 23:10:50 +0300

    - ffmpeg wrapper modified - CutVideo method was introduced - ffmpeg wrapper settings - configs were fixed

--- Heedbook/HBOperations/UserService/Program.cs
+++ Heedbook/HBOperations/UserService/Program.cs
@@ -14,6 +14,7 @@ namespace UserService
         public static IWebHostBuilder CreateWebHostBuilder(String[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
+                          .UseUrls("http://localhost:5050")
                           .UseStartup<Startup>();
         }
     }
