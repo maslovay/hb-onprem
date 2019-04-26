5bb91d4cfa16223de0e97f9f7dcd0719a1d9e003
Khamitov Daniyar <khamitov.dan@gmail.com>
2019-04-26 21:15:15 +0300

    fix

--- Heedbook/HBLib/Utils/FFMpegSettings.cs
+++ Heedbook/HBLib/Utils/FFMpegSettings.cs
@@ -5,6 +5,11 @@ namespace HBLib.Utils
     public class FFMpegSettings
     {
         public String LocalVideoPath { get; set; }
+
+        public String LocalFramesPath { get; set; }
+
+        public String LocalTempPath { get; set; }
+
         public String FFMpegPath { get; set; }
     }
 }
