5115aaeed57c6e0ad3e51c7bbf0781a3002da901
kertak90 <kertak@yandex.ru>
2019-05-06 18:21:06 +0300

    added reference controller

--- Heedbook/HBAPI/ReferenceController/Controllers/ReferenceController.cs
+++ Heedbook/HBAPI/ReferenceController/Controllers/ReferenceController.cs
@@ -22,9 +22,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using System.Net.Http;
 using System.Net;
+using FileRef.Models;
 using Newtonsoft.Json;
 using HBData;
 using HBData.Models;
+using HBLib;
+using HBLib.Utils;
 using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Query.Expressions;
@@ -37,60 +40,46 @@ namespace ReferenceController
     public class FileRefController : Controller
     {
         private IConfiguration _conf;
-        public FileRefController(IConfiguration conf)
+
+        private readonly SftpClient _client;
+        public FileRefController(IConfiguration conf,
+            SftpClient client)
         {
             _conf = conf;
+            _client = client;
         }
         //https://localhost:5001/FileRef/GetFile?path=/test/2.png&exp=2019-04-25T18:10:29&token=3bf12646e0941dec6d81c3b35470425e
         [HttpGet("GetFile")]
-        public IActionResult GetFile([FromQuery(Name = "path")] string path,
+        public async Task<IActionResult> GetFile([FromQuery(Name = "path")] string path,
                                     [FromQuery(Name = "exp")] DateTime exp,
                                     [FromQuery(Name = "token")] string token)
         {
             string hash = Methods.MakeExpiryHash(exp);
             if (String.IsNullOrEmpty(token))
                 return BadRequest();
-            if (token == hash)
+            if (token != hash) 
+                return BadRequest();
+            if (exp.ToUniversalTime() < DateTime.UtcNow)
+                return BadRequest();
+            if (!await _client.IsFileExistsAsync(path)) 
+                return BadRequest();
+            try
             {
-                if (exp.ToUniversalTime() < DateTime.UtcNow)
-                    return BadRequest();
-                else
+                using (var ms = await _client.DownloadFromFtpAsMemoryStreamAsync(path))
                 {
-                    var host = "40.87.147.2";
-                    var pass = "kloppolk_2018";
-                    var user = "nkrokhmal";
-
-                    using (var ms = new MemoryStream())
-                    {
-                        using (var sftp = new SftpClient(host, user, pass))
-                        {
-                            try
-                            {
-                                sftp.Connect();
-                                var storage = "/home/nkrokhmal/storage";
-                                var resultPath = storage + path;
-                                if (sftp.Exists(resultPath))
-                                    sftp.DownloadFile(resultPath, ms);
-                                else
-                                    return BadRequest();
-                                sftp.Disconnect();
-                                if (ms.Length == 0)
-                                    return BadRequest();
-                                ms.Position = 0;
-                            }
-                            catch (Exception ex)
-                            {
-                                sftp.Disconnect();
-                                Console.WriteLine(ex.ToString());
-                            }
-                        }
-                        var fileName = path.Split("/").LastOrDefault();
-                        var contentType = "application/octet-stream";
-                        return File(ms, contentType, fileName);
-                    }
+                    if (ms.Length == 0)
+                        return BadRequest();
+                    ms.Position = 0;
+                    var fileName = path.Split("/").LastOrDefault();
+                    var contentType = "application/octet-stream";
+                    return File(ms, contentType, fileName);
                 }
             }
-            return BadRequest();
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return BadRequest();
+            }
         }
 
         [HttpGet("GetReference")]
