61906ed5ebb48a7a3269a1000fa17ceadcd4327d
annushka34 <annsamoluk82@gmail.com>
2019-05-22 16:14:25 +0300

    fixes Roles include

--- Heedbook/HBAPI/HeedbookAPI/Controllers/UserController.cs
+++ Heedbook/HBAPI/HeedbookAPI/Controllers/UserController.cs
@@ -82,7 +82,7 @@ namespace UserOperations.Controllers
                 if (!_loginService.GetDataFromToken(Authorization, out userClaims))
                     return BadRequest("Token wrong");
                 var companyId = Guid.Parse(userClaims["companyId"]);
-                var users = _context.ApplicationUsers.Include(p => p.UserRoles)
+                var users = _context.ApplicationUsers.Include(p => p.UserRoles).ThenInclude(x => x.Role)
                     .Where(p => p.CompanyId == companyId && p.StatusId == activeStatus || p.StatusId == disabledStatus).ToList();     //2 active, 3 - disabled        
                 var result = users.Select(p => new UserModel(p, p.Avatar!= null? _sftpClient.GetFileLink(_containerName, p.Avatar, default(DateTime)).path: null));
                 return Ok(result);
