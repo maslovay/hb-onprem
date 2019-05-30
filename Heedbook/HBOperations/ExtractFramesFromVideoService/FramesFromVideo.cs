780470f4c97ad384589b1821a058cfe520db21f7
nkrokhmal <krokhmal11@mail.ru>
2019-05-30 16:19:07 +0300

    Fix

--- Heedbook/HBOperations/ExtractFramesFromVideoService/FramesFromVideo.cs
+++ Heedbook/HBOperations/ExtractFramesFromVideoService/FramesFromVideo.cs
@@ -1,6 +1,7 @@
 using HBData.Models;
 using HBData.Repository;
 using HBLib.Utils;
+using Newtonsoft.Json;
 using Notifications.Base;
 using RabbitMqEventBus.Events;
 using Renci.SshNet.Common;
@@ -57,7 +58,9 @@ namespace ExtractFramesFromVideo
                 using (var ftpDownloadStream =
                     await _client.DownloadFromFtpAsMemoryStreamAsync(videoBlobRelativePath))
                 {
+                    _log.Info($"File length - {ftpDownloadStream.Length}, File time stamp - {videoTimeStamp}");
                     var uploadStreams = await _wrapper.CutVideo(ftpDownloadStream, videoTimeStamp, appUserId, 10, 3);
+                    _log.Info($"Keys - {JsonConvert.SerializeObject(uploadStreams.Keys)}");
 
                     var uploadTasks = new List<Task>();
                     var insertToDbTasks = new List<Task>();
