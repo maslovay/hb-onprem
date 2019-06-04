2587b217633fdac5c4c9740b6a6bbf275314a5c1
nkrokhmal <krokhmal11@mail.ru>
2019-06-04 18:00:01 +0300

    Fix

--- Heedbook/HBOperations/UserService/Controllers/VideoSaveInfoController.cs
+++ Heedbook/HBOperations/UserService/Controllers/VideoSaveInfoController.cs
@@ -44,7 +44,7 @@ namespace UserService.Controllers
             try
             {   
                 _log.Info("Function Video save info started");
-                duration = duration == null ? 15 : duration;
+                duration = duration == null ? 15 * 1000 : duration;
                 var languageId = _context.ApplicationUsers
                                          .Include(p => p.Company)
                                          .Include(p => p.Company.Language)
@@ -59,7 +59,7 @@ namespace UserService.Controllers
                     ApplicationUserId = applicationUserId,
                     BegTime = time,
                     CreationTime = DateTime.UtcNow,
-                    Duration = duration,
+                    Duration = duration / 1000,
                     EndTime = time.AddMilliseconds((Double) duration),
                     FileContainer = "videos",
                     FileExist = await _sftpClient.IsFileExistsAsync($"videos/{fileName}"),
