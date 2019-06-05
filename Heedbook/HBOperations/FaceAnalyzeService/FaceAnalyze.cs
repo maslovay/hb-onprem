b094ddbc373b761e2694d9ba6465ca9bea78e761
Khamitov Daniyar <khamitov.dan@gmail.com>
2019-06-05 18:34:57 +0300

    fix face analyze

--- Heedbook/HBOperations/FaceAnalyzeService/FaceAnalyze.cs
+++ Heedbook/HBOperations/FaceAnalyzeService/FaceAnalyze.cs
@@ -19,7 +19,7 @@ namespace FaceAnalyzeService
         private readonly ElasticClient _log;
         private readonly IGenericRepository _repository;
         private readonly SftpClient _sftpClient;
-
+        private readonly Object _syncRoot = new Object();
         public FaceAnalyze(
             SftpClient sftpClient,
             IServiceScopeFactory factory,
@@ -40,7 +40,10 @@ namespace FaceAnalyzeService
                 //_log.Info($"{remotePath}");
                 if (await _sftpClient.IsFileExistsAsync(remotePath))
                 {
-                    var localPath = await _sftpClient.DownloadFromFtpToLocalDiskAsync(remotePath);
+                    string localPath;
+                    lock (_syncRoot) {
+                        localPath = _sftpClient.DownloadFromFtpToLocalDiskAsync(remotePath).GetAwaiter().GetResult();
+                    }
                     // _log.Info($"Download to path - {localPath}");
 
                     // var fileStream = File.OpenRead(localPath);
