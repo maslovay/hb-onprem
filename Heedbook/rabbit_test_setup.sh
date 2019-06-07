bf771dfdf3cbf1f10b039f4e6064bcf5fcc70901
Igor Evdokimov <igevdo@mail.ru>
2019-06-08 01:10:12 +0300

    Update rabbit_test_setup.sh

--- Heedbook/rabbit_test_setup.sh
+++ Heedbook/rabbit_test_setup.sh
@@ -1,33 +1,11 @@
 export DEBIAN_FRONTEND=noninteractive
-wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-dpkg -i erlang-solutions_1.0_all.deb
 
-
-## Install RabbitMQ signing key
-apt-key adv --keyserver "hkps.pool.sks-keyservers.net" --recv-keys "0x6B73A36E6026DFCA"
-
-## Install apt HTTPS transport
-apt-get install apt-transport-https
-
-## Add repositories that provision latest RabbitMQ and Erlang 21.x releases
-#wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-#dpkg -i erlang-solutions_1.0_all.deb
-#tee /etc/apt/sources.list.d/bintray.rabbitmq.list <<EOF
-
-
-wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | apt-key add -
-echo "deb https://packages.erlang-solutions.com/ubuntu bionic contrib" | tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
-deb https://dl.bintray.com/rabbitmq-erlang/debian bionic erlang-21.x
-deb https://dl.bintray.com/rabbitmq/debian bionic main
-EOF
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
 
 ## Update package indices
 apt-get update -y
 
-## Erlang installation
-apt-get install esl-erlang -y
-
 ## Install rabbitmq-server and its dependencies
 apt-get install rabbitmq-server -y --fix-missing --allow-unauthenticated
 apt-get install systemd -y
-
