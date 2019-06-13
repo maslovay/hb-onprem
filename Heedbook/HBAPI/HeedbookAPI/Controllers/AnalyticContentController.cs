2791e39fa80dc2224afbdd5048b7581f260b15a9
annushka34 <annsamoluk82@gmail.com>
2019-06-13 14:49:32 +0300

    adv efficiency (pool)

--- Heedbook/HBAPI/HeedbookAPI/Controllers/AnalyticContentController.cs
+++ Heedbook/HBAPI/HeedbookAPI/Controllers/AnalyticContentController.cs
@@ -138,9 +138,10 @@ namespace UserOperations.Controllers
                 var answersAmount = slideShowSessionsAll.Where(p => p.IsPoll).Count();
                 //---all answers during dialogue--- 
                 var answers = _context.CampaignContentAnswers
-                        .Where(p => p.Time > dialogue.BegTime
-                        && p.Time < dialogue.EndTime
-                        && p.ApplicationUserId == dialogue.ApplicationUserId).ToList();
+                        .Where(p => slideShowSessionsAll
+                        .Select(x => x.CampaignContentId)
+                        .Distinct()
+                        .Contains(p.CampaignContentId )).ToList();
                 //---answers grouping by content---                  
                 var answersByContent = pollAmount.Where(x => x.Key2 != null).Select(x => new
                 {
@@ -257,9 +258,9 @@ namespace UserOperations.Controllers
                 Console.WriteLine("------------LINE 255---------------");
 
                 var shows = slideShowSessionsAll.Count();
-                var views = slideShowSessionsAll.Where(x => x.DialogueId != null).Count();
+                var views = slideShowSessionsAll.Where(x => x.DialogueId != null && x.DialogueId != default(Guid)).Count();
                 var splashShows = slideShowSessionsAll.Where(x => x.CampaignContent!= null &&  x.CampaignContent.Campaign!= null && x.CampaignContent.Campaign.IsSplash).Count();
-                var clients = slideShowSessionsAll.Select(x => x.DialogueId).Distinct().Count();
+                var clients = slideShowSessionsAll.Where(x => x.DialogueId != null && x.DialogueId != default(Guid)).Select(x => x.DialogueId).Distinct().Count();
                
                 //-------------------------------GROUPING BY CONTENT (NOT POLL)--------------------------------
                 var contentsShownGroup = slideShowSessionsAll
@@ -280,7 +281,7 @@ namespace UserOperations.Controllers
                     ContentFullInfo = contentsShownGroup.Where(x => x.Key2 != null).Select(x => new ContentFullOneInfo
                     {
                         Content = x.Key2.ToString(),
-                        AmountViews = x.Result.Where(p => p.DialogueId != null).Count(),                            
+                        AmountViews = x.Result.Where(p =>  p.DialogueId != null && p.DialogueId != default(Guid)).Count(),                            
                         EmotionAttention = _dbOperation.SatisfactionDuringAdv(x.Result, dialogues),
                         Age = x.Result.Where(p => p.DialogueId != null).Average(p => p.Age),
                         Male = x.Result.Where(p => p.Gender == "male").Count(),
@@ -289,7 +290,7 @@ namespace UserOperations.Controllers
                     .Union(contentsShownGroup.Where(x => x.Key2 == null).Select(x => new ContentFullOneInfo
                     {
                         Content = x.Key1.ToString(),
-                        AmountViews = x.Result.Where(p => p.DialogueId != null).Count(),//TODO,
+                        AmountViews = x.Result.Where(p => p.DialogueId != null && p.DialogueId != default(Guid)).Count(),//TODO,
                         ContentName = x.Result.FirstOrDefault().ContentName,  
                         EmotionAttention = _dbOperation.SatisfactionDuringAdv(x.Result, dialogues),
                         Age = x.Result.Where(p => p.DialogueId != null).Average(p => p.Age),
@@ -303,6 +304,141 @@ namespace UserOperations.Controllers
                 var jsonToReturn = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(contentInfo));
 
                 _log.Info("ContentShows/ContentShows finished");
+                return Ok(jsonToReturn);
+            }
+            catch (Exception e)
+            {
+                _log.Fatal($"Exception occurred {e}");
+                return BadRequest(e);
+            }
+
+        }
+
+
+          [HttpGet("Poll")]
+        public IActionResult Poll([FromQuery(Name = "begTime")] string beg,
+                                                           [FromQuery(Name = "endTime")] string end,
+                                                        [FromQuery(Name = "applicationUserId[]")] List<Guid> applicationUserIds,
+                                                        [FromQuery(Name = "companyId[]")] List<Guid> companyIds,
+                                                        [FromQuery(Name = "corporationIds[]")] List<Guid> corporationIds,
+                                                        [FromQuery(Name = "campaignIds[]")] List<Guid> campaignIds,
+                                                        [FromQuery(Name = "contentIds[]")] List<Guid> contentIds,
+                                                        [FromHeader] string Authorization)
+        {
+            try
+            {
+                _log.Info("ContentShows/Poll started");
+                if (!_loginService.GetDataFromToken(Authorization, out var userClaims))
+                    return BadRequest("Token wrong");
+                var role = userClaims["role"];
+                var companyId = Guid.Parse(userClaims["companyId"]);
+                var begTime = _requestFilters.GetBegDate(beg);
+                var endTime = _requestFilters.GetEndDate(end);
+                _requestFilters.CheckRoles(ref companyIds, corporationIds, role, companyId);
+
+                var dialogues = _context.Dialogues
+                    .Include(p => p.ApplicationUser)
+                    .Include(p => p.DialogueClientSatisfaction)
+                    .Include(p => p.DialogueFrame)
+                    .Where(p => p.BegTime >= begTime
+                            && p.EndTime <= endTime
+                            //&& p.StatusId == 3
+                           // && p.InStatistic == true
+                            && (!companyIds.Any() || companyIds.Contains((Guid)p.ApplicationUser.CompanyId))
+                            && (!applicationUserIds.Any() || applicationUserIds.Contains(p.ApplicationUserId)))
+                    .Select(p => new DialogueInfoWithFrames
+                    {
+                        DialogueId = p.DialogueId,
+                        ApplicationUserId = p.ApplicationUserId,
+                        BegTime = p.BegTime,
+                        EndTime = p.EndTime,
+                        DialogueFrame = p.DialogueFrame.ToList(),
+                        Gender = p.DialogueClientProfile.Max(x => x.Gender),
+                        Age = p.DialogueClientProfile.Average(x => x.Age)
+
+                    })
+                    .ToList();
+                Console.WriteLine("-----------------------------3----------------------");
+
+                //-------------------------------ALL CONTENT SESSIONS POLL--------------------------------
+                var slideShowSessionsAll = _context.SlideShowSessions.Where(p => p.IsPoll)
+                    .Include(p => p.ApplicationUser)
+                    .Include(p => p.CampaignContent)
+                    .ThenInclude(p => p.Content)
+                    .Include(p => p.CampaignContent)
+                    .ThenInclude(p => p.Campaign)
+                    .Where(p => p.BegTime >= begTime
+                             && p.BegTime <= endTime
+                             && (!companyIds.Any() || companyIds.Contains((Guid)p.ApplicationUser.CompanyId))
+                             && (!applicationUserIds.Any() || applicationUserIds.Contains((Guid)p.ApplicationUserId))
+                             && (!campaignIds.Any() || campaignIds.Contains((Guid)p.CampaignContent.CampaignId))
+                             && (!contentIds.Any() || contentIds.Contains((Guid)p.CampaignContent.ContentId))
+                             && p.CampaignContent != null)
+                             .Select(p =>
+                                 new SlideShowInfo
+                                 {
+                                     BegTime = p.BegTime,
+                                     ContentId = p.CampaignContent.ContentId,
+                                     CampaignContent = p.CampaignContent,
+                                     ContentType = p.ContentType,
+                                     ContentName = p.CampaignContent.Content.Name,
+                                     EndTime = p.EndTime,
+                                     ApplicationUserId = (Guid)p.ApplicationUserId
+                                 }
+                             )
+                            .ToList();
+
+                Console.WriteLine("------------LINE 249---------------");
+                foreach ( var session in slideShowSessionsAll )
+                {
+                    var dialog =  dialogues.Where(x => x.BegTime <= session.BegTime &&  x.EndTime >= session.BegTime)
+                            .FirstOrDefault();
+                    session.DialogueId = dialog?.DialogueId;
+                }
+                 var answersList = _context.CampaignContentAnswers
+                        .Where(p => slideShowSessionsAll
+                        .Select(x => x.CampaignContent.CampaignContentId)
+                        .Distinct()
+                        .Contains(p.CampaignContentId )).ToList();
+                Console.WriteLine("------------LINE 255---------------");
+
+                var views = slideShowSessionsAll.Where(x => x.DialogueId != null && x.DialogueId != default(Guid)).Count();
+                var clients = slideShowSessionsAll
+                    .Where(x => x.DialogueId != null && x.DialogueId != default(Guid)).Select(x => x.DialogueId).Distinct().Count();
+                var answers = answersList.Count();
+                double conversion = views != 0 ? (double)answers / (double)views : 0;
+               
+                //-------------------------------GROUPING BY CONTENT (NOT POLL)--------------------------------
+                var contentsShownGroup = slideShowSessionsAll
+                    .GroupBy(p => p.ContentId).ToList();
+                    
+                Console.WriteLine("------------LINE 270---------------"); 
+                var contentInfo = new
+                {
+                    Views = views,
+                    Clients = clients,
+                    Answers = answers,
+                    Conversion = conversion,
+                    //---url---
+                    ContentFullInfo = contentsShownGroup.Select(x => new
+                    {
+                        Content = x.Key.ToString(),
+                        AmountViews = x.Where(p => p.DialogueId != null && p.DialogueId != default(Guid)).Count(),//TODO,
+                        ContentName = x.FirstOrDefault().ContentName,
+                        Answers = answersList
+                            .Where(p => x.Select(r => r.CampaignContent.CampaignContentId).Contains(p.CampaignContentId))
+                            .Select(p => new { p.Answer, p.Time }),
+                        answersAmount = answersList
+                            .Where(p => x.Select(r => r.CampaignContent.CampaignContentId).Contains(p.CampaignContentId))
+                            .Select(p => new { p.Answer, p.Time }).Count()
+                    }
+                    ).ToList()
+                };              
+                Console.WriteLine("------------LINE 289---------------");
+                Console.WriteLine("----------5----------------");
+                var jsonToReturn = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(contentInfo));
+
+                _log.Info("ContentShows/Poll finished");
                 return Ok(jsonToReturn);
             }
             catch (Exception e)
