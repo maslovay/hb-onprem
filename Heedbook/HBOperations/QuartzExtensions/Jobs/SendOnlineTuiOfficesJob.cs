c4bb01debba2fe8e9821ce6f461d399e135aa734
kertak90 <kertak@yandex.ru>
2019-06-19 18:09:14 +0300

    create TuiOnlineOfficesService

---
+++ Heedbook/HBOperations/QuartzExtensions/Jobs/SendOnlineTuiOfficesJob.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using HBData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace QuartzExtensions.Jobs
+{
+    public class SendOnlineTuiOfficesJob : IJob
+    {
+        private readonly RecordsContext _context;
+        private List<Guid?> tuiOfficeGuids = new List<Guid?>
+            {
+                new Guid("862b66a8-8d09-4363-8504-d5c2dce15e15"),
+                new Guid("82560395-2cc3-46e8-bcef-c844f1048182"),
+                new Guid("3aacfc92-583f-4f3f-bb3b-e73d933008d7"),
+                new Guid("6024892a-f535-46d7-bfc6-d926ee16461c"),
+                new Guid("aae8058f-b9a6-4c6a-87f9-3fc78e46eebf"),
+                new Guid("b7b60c4c-47bc-45d8-8a8c-11fb12f6047c"),
+                new Guid("b7b60c4c-47bc-45d8-8a8c-11fb12f6047c"),
+                new Guid("bf51875d-2331-4a57-b30d-a12cff20beab"),
+                new Guid("a1e2f8f0-388f-45d7-9dcd-cd5ed8e22436"),
+                new Guid("9e55e308-1b96-4d0c-8ca7-cfe7bf26ff82"),
+                new Guid("3f1a1253-238c-429c-b319-2c03f95140aa"),
+                new Guid("e99c8fe4-3835-4ff4-8f32-7694290492ab"),
+                new Guid("690f03f2-6565-413a-a658-227140780c75"),
+                new Guid("7b83800e-583a-49a6-9e13-775a095f5baa"),
+                new Guid("fbc2d85e-1f32-4521-8ac4-a184ee71b554")
+            };
+        public SendOnlineTuiOfficesJob(IServiceScopeFactory factory)
+        {            
+            _context = factory.CreateScope().ServiceProvider.GetService<RecordsContext>();            
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {            
+            var mail = new MailMessage();
+            mail.From = new MailAddress("support@heedbook.com");
+            mail.To.Add(new MailAddress("anisiya.kobylina@tui.ru"));            
+            
+            mail.Subject = "Активные офисы TUI";
+            var data = TuiOnlineOffices();
+            var mailData = "";
+            foreach (var item in data) mailData += item.CompanyName + " " + item.ApplicationUserName + " Online" + "\n";
+
+            mail.Body = mailData;
+            mail.IsBodyHtml = false;
+            //mail.Attachments.Add(new Attachment("/home/oleg/Документы/My_Saves/save1.txt"));
+
+            //For Yandex
+            var host = "smtp.yandex.ru";
+            var port = 25;
+
+            var smtpClient = new SmtpClient(host, port);
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+
+            var password = "Heedbook_2017";
+
+            smtpClient.Credentials = new NetworkCredential("support@heedbook.com", password);
+            smtpClient.Send(mail);
+            
+        }
+
+        public List<CompanyInformation> TuiOnlineOffices()
+        {       
+            
+            var offices = _context.Sessions
+                                .Include(p=>p.ApplicationUser)
+                                .Include(p=>p.ApplicationUser.Company)
+                                .Where(p=>p.StatusId == 6
+                                &&tuiOfficeGuids.Contains(p.ApplicationUser.CompanyId))
+                                .GroupBy(p => p.ApplicationUser.CompanyId)
+                                .Select(p=> new CompanyInformation
+                                    {
+                                        CompanyId = p.Key,
+                                        CompanyName = p.First().ApplicationUser.Company.CompanyName,
+                                        ApplicationUserName = p.First().ApplicationUser.FullName
+                                    })
+                                .ToList();            
+            return offices;
+        }
+    }
+
+    public class CompanyInformation
+    {
+        public Guid? CompanyId { get; set; }
+        public String CompanyName { get; set; }    
+        public String ApplicationUserName {get; set;}   
+    }
+}
