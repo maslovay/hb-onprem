03407ee14436890120f48813fb35fc02671c31b3
nkrokhmal <krokhmal11@mail.ru>
2019-07-11 18:29:44 +0300

    Fix

---
+++ Heedbook/HBOperations/DialogueVideoAssembleService/DialogueVideoAssembleSettings.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DialogueVideoAssembleService
+{
+    public class DialogueVideoAssembleSettings
+    {
+        public String VideoFolder { get; set; }
+        public String FrameFolder { get; set; }
+        public String VideoType { get; set; }
+        public String FrameType {get;set;}
+    }
+}
