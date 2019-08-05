9deddba9edb80c83e55d4cc9954801d957c98c20
devgopher <igevdo@mail.ru>
2019-08-05 11:37:39 +0300

    - Express api

---
+++ Heedbook/ExpressHbApiTester/HbApiTester/Controllers/ExpressTesterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlarmSender;
+using HbApiTester;
+using HbApiTester.Settings;
+using HbApiTester.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ExpressTesterWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpressTesterController : ControllerBase
+    {
+        private readonly CommandManager _commandManager;
+
+
+        public ExpressTesterController(CommandManager commandManager)
+            => _commandManager = commandManager;
+
+        [HttpGet("[action]")]
+        public ActionResult<ObjectResult> StartApiTests()
+        {
+            try
+            {
+                _commandManager.RunCommand("/api_tests");
+                return Ok("Api tests started!");
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult("Exception occurred: "  + ex.Message);
+            }
+        }
+    }
+}
