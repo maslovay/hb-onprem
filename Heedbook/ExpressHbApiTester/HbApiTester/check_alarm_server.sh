3fd51b0646cdf7714561e3e22532fbf9cefc852b
Ubuntu <heedbookadmin@AlarmServer.zycjqrgg4kfufbnesx54pbptma.ux.internal.cloudapp.net>
2019-08-09 14:17:27 +0000

    alarmServer watchdog

---
+++ Heedbook/ExpressHbApiTester/HbApiTester/check_alarm_server.sh
@@ -0,0 +1,9 @@
+command="HbApiTester.dll"
+running=`ps ax | grep -v grep | grep $command | wc -l`
+if [ $running -gt 0 ]; then
+    echo "Dotnet is running"
+else
+    echo "Dotnet is not running! Truing to restarrt alarm server!"
+    cd ~/sources/hb-onprem/Heedbook/ExpressHbApiTester/HbApiTester/
+    dotnet run
+fi
