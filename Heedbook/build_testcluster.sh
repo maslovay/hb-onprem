9a84102945fe635c8da66578b74809821138f02f
devgopher <igevdo@mail.ru>
2019-08-19 14:00:11 +0300

    build preprare for pilelines

--- Heedbook/build_testcluster.sh
+++ Heedbook/build_testcluster.sh
@@ -1,31 +1,6 @@
 #!/usr/bin/env bash
-# sed -i "s/latest/$1/" PersonDetectionService/person-detection-service-deployment.yaml
 
-rm -rf tmp
-mkdir tmp
-cp *.Dockerfile* tmp
-
-sed -i "s/testcluster/true/" tmp/FaceAnalyzeService.Dockerfile  
-sed -i "s/testcluster/true/" tmp/UserService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/UserOperations.Dockerfile 
-sed -i "s/testcluster/true/" tmp/FillingFrameService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/ExtractFramesFromVideoService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/VideoToSoundService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/AudioAnalyzeService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/ToneAnalyzeService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/AudioAnalyzeScheduler.Dockerfile 
-sed -i "s/testcluster/true/" tmp/DialogueStatusCheckerScheduler.Dockerfile 
-sed -i "s/testcluster/true/" tmp/DialogueVideoMergeService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/FillingHintService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/FillingSatisfactionService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/DialogueVideoAssembleService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/DialogueMarkUpService.Dockerfile 
-sed -i "s/testcluster/true/" tmp/SessionStatusScheduler.Dockerfile 
-sed -i "s/testcluster/true/" tmp/OnlineTuiOfficesScheduler.Dockerfile 
-sed -i "s/testcluster/true/" tmp/HeedbookDevelopmentStatistics.Dockerfile 
-sed -i "s/testcluster/true/" tmp/DialoguesRecalculateScheduler.Dockerfile 
-sed -i "s/testcluster/true/" tmp/SendUserAnalyticReportScheduler.Dockerfile 
-sed -i "s/testcluster/true/" tmp/ReferenceController.Dockerfile 
+sh ./build_prepare_testcluster.sh
 
 docker build -t hbtestregistry.azurecr.io/faceanalyzeservice:latest -f tmp/FaceAnalyzeService.Dockerfile . &&
 docker build -t hbtestregistry.azurecr.io/userservice:latest -f tmp/UserService.Dockerfile . && 
