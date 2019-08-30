a9ad1f95f479554093fe962bc9a79a66b0e5c33d
Ann <annsamoluk82@gmail.com>
2019-08-30 11:56:43 +0300

    add worker types create with company (#392)

--- Heedbook/HBAPI/HeedbookAPI/Controllers/TestController.cs
+++ Heedbook/HBAPI/HeedbookAPI/Controllers/TestController.cs
@@ -1,75 +1,69 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System.IO;
-using Microsoft.AspNetCore.Http;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using Microsoft.Extensions.Configuration;
-using HBData.Models;
-using HBData.Models.AccountViewModels;
-using UserOperations.Services;
-using UserOperations.AccountModels;
 using HBData;
-
-///REMOVE IT
-using System.Data.SqlClient;
-
-
-using System.Globalization;
+using UserOperations.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using System.Net.Http;
-using System.Net;
 using Newtonsoft.Json;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.WindowsAzure.Storage;
-using HBLib.Utils;
 using UserOperations.Utils;
-using Npgsql;
-using System.Threading;
-using BenchmarkDotNet.Attributes;
+using Swashbuckle.AspNetCore.Annotations;
 
-namespace UserOperations.Controllers
+namespace UserOperations.Controllers.Test
 {
     [Route("api/[controller]")]
     [ApiController]
     public class TestController : Controller
     {
+        private readonly IConfiguration _config;
+        private readonly ILoginService _loginService;
         private readonly RecordsContext _context;
+        private readonly DBOperations _dbOperation;
+        private readonly RequestFilters _requestFilters;
+        private readonly List<AgeBoarder> _ageBoarders;
+        // private readonly ElasticClient _log;
 
-        public TestController ( RecordsContext context )
+        public TestController(
+            IConfiguration config,
+            ILoginService loginService,
+            RecordsContext context,
+            DBOperations dbOperation,
+            RequestFilters requestFilters
+            // ElasticClient log
+            )
         {
+            _config = config;
+            _loginService = loginService;
             _context = context;
-        }     
-
-        [HttpGet("TestToList")]
-        public IActionResult TestToList( [FromQuery]Guid id)
-        {
-            var session = _context.Sessions
-                       .Where(p => p.ApplicationUserId == id)
-                       .ToList()?.OrderByDescending(p => p.BegTime);
-            return Ok(session);
+            _dbOperation = dbOperation;
+            _requestFilters = requestFilters;
+        
         }
 
-        [HttpGet("TestWithoutToList")]
-        public IActionResult TestWithoutToList([FromQuery]string id)
+
+        [HttpGet("Origin")]
+        public IActionResult EfficiencyDashboardOrigin([FromQuery(Name = "begTime")] string beg,
+                                                        [FromQuery(Name = "endTime")] string end,
+                                                        [FromQuery(Name = "applicationUserId[]")] List<Guid> applicationUserIds,
+                                                        [FromQuery(Name = "companyId[]")] List<Guid> companyIds,
+                                                        [FromQuery(Name = "corporationIds[]")] List<Guid> corporationIds,
+                                                        [FromQuery(Name = "workerTypeId[]")] List<Guid> workerTypeIds,
+                                                        [FromHeader] string Authorization)
         {
-            var session = _context.Sessions
-                       .Where(p => p.ApplicationUserId.ToString() == id)?
-                       .OrderByDescending(p => p.BegTime).ToList();
-            return Ok(session);
+            return Ok();
+        }
+
+        [HttpGet("Test")]
+        public IActionResult EfficiencyDashboardTest([FromQuery(Name = "begTime")] string beg,
+                                                    [FromQuery(Name = "endTime")] string end,
+                                                    [FromQuery(Name = "applicationUserId[]")] List<Guid> applicationUserIds,
+                                                    [FromQuery(Name = "companyId[]")] List<Guid> companyIds,
+                                                    [FromQuery(Name = "corporationIds[]")] List<Guid> corporationIds,
+                                                    [FromQuery(Name = "workerTypeId[]")] List<Guid> workerTypeIds,
+                                                    [FromHeader] string Authorization)
+        {
+            return Ok();
         }
 
     }
