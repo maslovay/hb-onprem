ca731d936309c59af6d61462979c8c1170bd966a
kertak90 <kertak@yandex.ru>
2019-11-06 19:10:17 +0300

    added FrameAnalyze method

--- Heedbook/HBOperations/UserService/Controllers/FaceController.cs
+++ Heedbook/HBOperations/UserService/Controllers/FaceController.cs
@@ -1,5 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using HBMLHttpClient;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Notifications.Base;
 using RabbitMqEventBus.Events;
 using Swashbuckle.AspNetCore.Annotations;
@@ -11,10 +17,12 @@ namespace UserService.Controllers
     public class FaceController : ControllerBase
     {
         private readonly INotificationHandler _handler;
+        private readonly HbMlHttpClient _client;
 
-        public FaceController(INotificationHandler handler)
+        public FaceController(INotificationHandler handler, HbMlHttpClient client)
         {
             _handler = handler;
+            _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
         [HttpPost]
@@ -23,6 +31,47 @@ namespace UserService.Controllers
         public async Task FaceAnalyzeRun([FromBody] FaceAnalyzeRun message)
         {
             _handler.EventRaised(message);
+        }
+
+        [HttpPost("[action]")]
+        [SwaggerOperation(Description = "Analyze frame. Detect face, return gender and age")]
+        public async Task<IActionResult> FrameAnalyze([FromForm] IFormCollection formData)
+        {
+            try
+            {
+                var memoryStream = formData.Files.FirstOrDefault().OpenReadStream();
+                var byteArray = StreamToByteArray(memoryStream);
+                var base64String = Convert.ToBase64String(byteArray);
+                var faceResult = await _client.GetFaceResult(base64String);                
+                //System.Console.WriteLine(faceResult);
+                //_handler.EventRaised(message);
+                var result = faceResult.Select(p => new 
+                    {
+                        p.Attributes.Age,
+                        p.Attributes.Gender
+                    });
+                var jsonResult = JsonConvert.SerializeObject(result);
+                System.Console.WriteLine(jsonResult);
+                return Ok(jsonResult);
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return BadRequest(ex.Message);
+            }            
+        }
+        private byte[] StreamToByteArray(Stream input)
+        {
+            byte[] buffer = new byte[16*1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
         }
     }
 }
