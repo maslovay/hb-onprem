cbfd949a3a3eac2efe7c3facb3fdcdc0f7be5b0d
nkrokhmal <krokhmal11@mail.ru>
2019-11-28 10:46:44 +0300

    Fix

--- Heedbook/HBOperations/UserService/Controllers/PersonDetectionController.cs
+++ Heedbook/HBOperations/UserService/Controllers/PersonDetectionController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Notifications.Base;
 using RabbitMqEventBus.Events;
 using Swashbuckle.AspNetCore.Annotations;
+using HBData;
+using HBData.Models;
+using HBData.Repository;
+using System;
+using System.Linq;
 
 namespace UserService.Controllers
 {
@@ -11,16 +16,31 @@ namespace UserService.Controllers
     public class PersonDetectionController : ControllerBase
     {
         private readonly INotificationHandler _handler;
-
-        public PersonDetectionController(INotificationHandler handler)
+        private readonly RecordsContext _context;
+        public PersonDetectionController(INotificationHandler handler, RecordsContext context )
         {
             _handler = handler;
+            _context = context;
         }
 
         [HttpPost]
         [SwaggerOperation(Description = "Calculate dialogue satisfaction score")]
         public async Task PersonDetectionRun([FromBody] PersonDetectionRun message)
         {
+            _handler.EventRaised(message);
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Description = "Calculate dialogue satisfaction score")]
+        public async Task PersonDetectionAllUsersRun()
+        {
+            
+            var begTime = DateTime.UtcNow.AddDays(-30);
+            var users = _context.Dialogues.Where(p => p.BegTime > begTime)
+                .Select(p => p.ApplicationUserId)
+                .ToList();
+            var message = new PersonDetectionRun();
+            message.ApplicationUserIds = users;
             _handler.EventRaised(message);
         }
     }
