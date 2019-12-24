9071bf3e02ea227740b1847a9d87ec217f1ab371
annushka34 <annsamoluk82@gmail.com>
2019-12-24 15:33:53 +0200

    catalogue change

--- Heedbook/HBAPI/HeedbookAPI/Controllers/CatalogueController.cs
+++ Heedbook/HBAPI/HeedbookAPI/Controllers/CatalogueController.cs
@@ -1,26 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System.IO;
-using Microsoft.AspNetCore.Http;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.Extensions.Configuration;
-using UserOperations.AccountModels;
 using HBData.Models;
-using HBData.Models.AccountViewModels;
 using UserOperations.Services;
-using HBData;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace UserOperations.Controllers
@@ -28,6 +10,7 @@ namespace UserOperations.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [AllowAnonymous]
+    [ControllerExceptionFilter]
     public class CatalogueController : Controller
     {        
         private readonly CatalogueService _catalogueService;
@@ -39,44 +22,38 @@ namespace UserOperations.Controllers
 
         [HttpGet("Country")]
         [SwaggerOperation(Description = "Return all countries. Does not require to transfer a token")]
-        [AllowAnonymous]
         public IEnumerable<Country> CountrysGet() => 
             _catalogueService.CountrysGet();
         
         [HttpGet("Role")]
         [SwaggerOperation(Description = "Return all available user roles. Does not require to transfer a token")]
-        [AllowAnonymous]
         public IEnumerable<ApplicationRole> RolesGet() =>
             _catalogueService.RolesGet();
 
         [HttpGet("WorkerType")]
         [SwaggerOperation(Summary = "Return worker types", Description = "Return all available worker types for company with id ('Кассир','Кредитный менеджер' и др). Require to transfer a token")]
         [SwaggerResponse(200, "Content", typeof(WorkerType))]
-        [AllowAnonymous]
-        public IEnumerable<object> WorkerTypeGet([FromHeader] string Authorization) =>
-            _catalogueService.WorkerTypeGet(Authorization);
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public IEnumerable<object> WorkerTypeGet() =>
+            _catalogueService.WorkerTypeGet();
 
         [HttpGet("Industry")]
         [SwaggerOperation(Description = "Return all industries. Does not require to transfer a token")]
-        [AllowAnonymous]
         public IEnumerable<CompanyIndustry> IndustryGet() =>
             _catalogueService.IndustryGet();
 
         [HttpGet("Language")]
         [SwaggerOperation(Description = "Return all available languages. Does not require to transfer a token")]
-        [AllowAnonymous]
         public IEnumerable<Language> LanguageGet() =>
             _catalogueService.LanguageGet();
 
         [HttpGet("PhraseType")]
         [SwaggerOperation(Description = "Return all available phrase types. Does not require to transfer a token")]
-        [AllowAnonymous]
         public IEnumerable<PhraseType> PhraseTypeGet() =>
             _catalogueService.PhraseTypeGet();
 
         [HttpGet("AlertType")]
         [SwaggerOperation(Description = "Return all available alert types. Does not require to transfer a token")]
-        [AllowAnonymous]
         public IEnumerable<AlertType> AlertTypeGet() =>
             _catalogueService.AlertTypeGet();
     }
--- Heedbook/HBAPI/HeedbookAPI/Services/CatalogueService.cs
+++ Heedbook/HBAPI/HeedbookAPI/Services/CatalogueService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
 using HBData.Models;
 using HBData.Repository;
 
@@ -19,41 +18,39 @@ namespace UserOperations.Services
             )
         {
             _loginService = loginService;
-            // _log = log;
             _repository = repository;
         }
         public IEnumerable<Country> CountrysGet()
         {
-            // _log.Info("Catalogue/Country GET");
             return _repository.GetAsQueryable<Country>().ToList();
         }
         
         public IEnumerable<ApplicationRole> RolesGet()
         {
-            // _log.Info("Catalogue/Role GET");
             return _repository.GetAsQueryable<ApplicationRole>().ToList();
         }
-        public IEnumerable<object> WorkerTypeGet([FromHeader] string Authorization)
+        public IEnumerable<object> WorkerTypeGet()
         {
-            // _log.Info("Catalogue/WorkerType GET");
-            if (!_loginService.GetDataFromToken(Authorization, out var userClaims))
-                return null;
-            var companyId = Guid.Parse(userClaims["companyId"]);
-            return _repository.GetAsQueryable<WorkerType>().Where(p => p.CompanyId == companyId).Select(p => new { p.WorkerTypeId, p.WorkerTypeName }).ToList();
+            try
+            {
+                var companyId = _loginService.GetCurrentCompanyId();
+                return _repository.GetAsQueryable<WorkerType>().Where(p => p.CompanyId == companyId).Select(p => new { p.WorkerTypeId, p.WorkerTypeName }).ToList();
+            }
+            catch
+            {
+                throw new UnauthorizedAccessException();
+            }
         }
         public IEnumerable<CompanyIndustry> IndustryGet()
         {
-            // _log.Info("Catalogue/Industry GET");
             return _repository.GetAsQueryable<CompanyIndustry>().ToList();
         }
         public IEnumerable<Language> LanguageGet()
         {
-            // _log.Info("Catalogue/Language GET");
             return _repository.GetAsQueryable<Language>().ToList();
         }
         public IEnumerable<PhraseType> PhraseTypeGet()
         {
-            // _log.Info("Catalogue/PhraseType GET");
             return _repository.GetAsQueryable<PhraseType>().ToList();
         }
         public IEnumerable<AlertType> AlertTypeGet()
