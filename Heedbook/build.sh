8613c49c665397fbf8a3ed4263f44d467dad5268
nkrokhmal <krokhmal11@mail.ru>
2020-01-13 15:58:15 +0300

    Fix

--- Heedbook/build.sh
+++ Heedbook/build.sh
@@ -1,48 +1,48 @@
 #!/usr/bin/env bash
-docker build -t heedbookcontainerregistry.azurecr.io/faceanalyzeservice:latest -f FaceAnalyzeService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/userservice:latest -f UserService.Dockerfile . && 
-docker build -t heedbookcontainerregistry.azurecr.io/useroperations:latest -f UserOperations.Dockerfile . && 
-docker build -t heedbookcontainerregistry.azurecr.io/fillingframeservice:latest -f FillingFrameService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/extractframesfromvideoservice:latest -f ExtractFramesFromVideoService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/videotosoundservice:latest -f VideoToSoundService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/audioanalyzeservice:latest -f AudioAnalyzeService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/toneanalyzeservice:latest -f ToneAnalyzeService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/audioanalyzescheduler:latest -f AudioAnalyzeScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/dialoguestatuscheckerscheduler:latest -f DialogueStatusCheckerScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/dialoguevideomergeservice:latest -f DialogueVideoMergeService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/fillinghintservice:latest -f FillingHintService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/fillingsatisfactionservice:latest -f FillingSatisfactionService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/dialoguevideoassembleservice:latest -f DialogueVideoAssembleService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/dialoguemarkupservice:latest -f DialogueMarkUpService.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/sessionstatusscheduler:latest -f SessionStatusScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/onlinetuiofficesscheduler:latest -f OnlineTuiOfficesScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/heedbookdevelopmentstatisticsscheduler:latest -f HeedbookDevelopmentStatistics.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/dialoguesrecalculatescheduler:latest -f DialoguesRecalculateScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/senduseranalyticreportscheduler:latest -f SendUserAnalyticReportScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/dialogueandsessionsnestedscheduler:latest -f DialogueAndSessionsNestedScheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/oldvideotoframecutsheduler:latest -f OldVideoToFrameCutSheduler.Dockerfile . &&
-docker build -t heedbookcontainerregistry.azurecr.io/logsaveservice:latest -f LogSaveService.Dockerfile .
+docker build -t heedbookcontainerregistrytest.azurecr.io/faceanalyzeservice:latest -f FaceAnalyzeService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/userservice:latest -f UserService.Dockerfile . && 
+docker build -t heedbookcontainerregistrytest.azurecr.io/useroperations:latest -f UserOperations.Dockerfile . && 
+docker build -t heedbookcontainerregistrytest.azurecr.io/fillingframeservice:latest -f FillingFrameService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/extractframesfromvideoservice:latest -f ExtractFramesFromVideoService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/videotosoundservice:latest -f VideoToSoundService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/audioanalyzeservice:latest -f AudioAnalyzeService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/toneanalyzeservice:latest -f ToneAnalyzeService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/audioanalyzescheduler:latest -f AudioAnalyzeScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/dialoguestatuscheckerscheduler:latest -f DialogueStatusCheckerScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/dialoguevideomergeservice:latest -f DialogueVideoMergeService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/fillinghintservice:latest -f FillingHintService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/fillingsatisfactionservice:latest -f FillingSatisfactionService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/dialoguevideoassembleservice:latest -f DialogueVideoAssembleService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/dialoguemarkupservice:latest -f DialogueMarkUpService.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/sessionstatusscheduler:latest -f SessionStatusScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/onlinetuiofficesscheduler:latest -f OnlineTuiOfficesScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/heedbookdevelopmentstatisticsscheduler:latest -f HeedbookDevelopmentStatistics.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/dialoguesrecalculatescheduler:latest -f DialoguesRecalculateScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/senduseranalyticreportscheduler:latest -f SendUserAnalyticReportScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/dialogueandsessionsnestedscheduler:latest -f DialogueAndSessionsNestedScheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/oldvideotoframecutsheduler:latest -f OldVideoToFrameCutSheduler.Dockerfile . &&
+docker build -t heedbookcontainerregistrytest.azurecr.io/logsaveservice:latest -f LogSaveService.Dockerfile .
 
-docker push heedbookcontainerregistry.azurecr.io/faceanalyzeservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/userservice:latest && 
-docker push heedbookcontainerregistry.azurecr.io/useroperations:latest && 
-docker push heedbookcontainerregistry.azurecr.io/fillingframeservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/extractframesfromvideoservice:latest && 
-docker push heedbookcontainerregistry.azurecr.io/videotosoundservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/audioanalyzeservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/toneanalyzeservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/dialoguestatuscheckerscheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/audioanalyzescheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/dialoguevideomergeservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/fillinghintservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/fillingsatisfactionservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/dialoguevideoassembleservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/dialoguemarkupservice:latest &&
-docker push heedbookcontainerregistry.azurecr.io/sessionstatusscheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/onlinetuiofficesscheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/heedbookdevelopmentstatisticsscheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/dialoguesrecalculatescheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/senduseranalyticreportscheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/dialogueandsessionsnestedscheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/oldvideotoframecutsheduler:latest &&
-docker push heedbookcontainerregistry.azurecr.io/logsaveservice:latest
+docker push heedbookcontainerregistrytest.azurecr.io/faceanalyzeservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/userservice:latest && 
+docker push heedbookcontainerregistrytest.azurecr.io/useroperations:latest && 
+docker push heedbookcontainerregistrytest.azurecr.io/fillingframeservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/extractframesfromvideoservice:latest && 
+docker push heedbookcontainerregistrytest.azurecr.io/videotosoundservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/audioanalyzeservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/toneanalyzeservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/dialoguestatuscheckerscheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/audioanalyzescheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/dialoguevideomergeservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/fillinghintservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/fillingsatisfactionservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/dialoguevideoassembleservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/dialoguemarkupservice:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/sessionstatusscheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/onlinetuiofficesscheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/heedbookdevelopmentstatisticsscheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/dialoguesrecalculatescheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/senduseranalyticreportscheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/dialogueandsessionsnestedscheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/oldvideotoframecutsheduler:latest &&
+docker push heedbookcontainerregistrytest.azurecr.io/logsaveservice:latest
