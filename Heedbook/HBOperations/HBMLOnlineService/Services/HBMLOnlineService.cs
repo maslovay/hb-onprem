1ea941f3de0c4b6bf5188437dd28d87e4e490442
nkrokhmal <krokhmal11@mail.ru>
2020-01-22 11:40:58 +0300

    Fix

--- Heedbook/HBOperations/HBMLOnlineService/Services/HBMLOnlineService.cs
+++ Heedbook/HBOperations/HBMLOnlineService/Services/HBMLOnlineService.cs
@@ -57,7 +57,7 @@ namespace HBMLOnlineService.Service
             var personOnlineDetectionRun = new PersonOnlineDetectionRun
             {
                 DeviceId = deviceId,
-                Attributes = JsonConvert.SerializeObject(faceResults.First().Attributes),
+                Attributes = JsonConvert.SerializeObject(faceResults.First().Rectangle),
                 Path = $"useravatars/{filename}",
                 CompanyId = companyId,
                 Descriptor = JsonConvert.SerializeObject(faceResults.First().Descriptor),
