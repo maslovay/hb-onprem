c85879d5ec90c8b2c4acae0a7f1ea50e4096f367
Oleg Pinarin <kertak@yandex.ru>
2020-01-28 11:23:05 +0300

    fix

--- Heedbook/HBOperations/IntegrationAPITestsService/Handler/IntegrationTestsRunHandler.cs
+++ Heedbook/HBOperations/IntegrationAPITestsService/Handler/IntegrationTestsRunHandler.cs
@@ -16,7 +16,7 @@ namespace IntegrationAPITestsService.Handler
 
         public async Task Handle(IntegrationTestsRun @event)
         {
-            System.Console.WriteLine($"event command: {@event.Command}");
+            System.Console.WriteLine($"IntegrationAPITestsServite take command: {@event.Command}");
             await _integrationTests.Run(@event.Command);
         }
     }
