035469a76a3ea162ef1eaddd2574435a28f01695
Oleg Pinarin <kertak@yandex.ru>
2020-02-10 12:32:55 +0300

    added dialogueIntersectionMoreThan80Percent

--- Heedbook/HBOperations/DialogueMarkUp/QuartzJobs/CheckDialogueMarkUpJob.cs
+++ Heedbook/HBOperations/DialogueMarkUp/QuartzJobs/CheckDialogueMarkUpJob.cs
@@ -52,7 +52,8 @@ namespace DialogueMarkUp.QuartzJobs
                 var endTime = DateTime.UtcNow.AddMinutes(-30);
                 var frameAttributes = _context.FrameAttributes
                     .Include(p => p.FileFrame)
-                    .Where(p => p.FileFrame.StatusNNId == 6 
+                    .Where(p => 
+                        p.FileFrame.StatusNNId == 6 
                         && p.FileFrame.Time > begMarkUpTime
                         && p.FileFrame.Time < endTime 
                         && p.FileFrame.FaceLength > 0)
@@ -227,6 +228,9 @@ namespace DialogueMarkUp.QuartzJobs
                         foreach (var updatedMarkUp in updatedMarkUps)
                         {   
                             var dialogueId = Guid.NewGuid();
+                            if(dialogueIntersectionMoreThan80Percent(applicationUserId, updatedMarkUp.BegTime, updatedMarkUp.EndTime))
+                                continue;
+                                
                             var dialogue = _classCreator.CreateDialogueClass(dialogueId, applicationUserId, updatedMarkUp.BegTime, 
                                 updatedMarkUp.EndTime, updatedMarkUp.Descriptor);
                             log.Info($"Create dialogue --- {dialogue.BegTime}, {dialogue.EndTime}, {dialogue.DialogueId}");
@@ -273,7 +277,36 @@ namespace DialogueMarkUp.QuartzJobs
                 _publisher.Publish(personDetection);
             }
         }
+        private bool dialogueIntersectionMoreThan80Percent(Guid applicationUserId, DateTime begTime, DateTime endTime)
+        {
+            var dialogues = _context.Dialogues.Where(p => p.ApplicationUserId == applicationUserId
+                    && (p.StatusId == 3 || p.StatusId == 6));
 
+            if(dialogues == null || dialogues.Count() == 0)
+                return false;
+            var intersectDialogues = dialogues.Where(p =>
+                    ((p.BegTime <= begTime
+                        && p.EndTime > begTime
+                        && p.EndTime < endTime) 
+                    || (p.BegTime < endTime
+                        && p.BegTime > begTime
+                        && p.EndTime >= endTime)
+                    || (p.BegTime >= begTime
+                        && p.EndTime <= endTime)
+                    || (p.BegTime < begTime
+                        && p.EndTime > endTime)));
+            
+            var moreThan80PersentIntersectDialogues = intersectDialogues.Where(p => 
+                    (MinTime(endTime, p.EndTime).Subtract(MaxTime(begTime,p.BegTime)).TotalSeconds 
+                        > endTime.Subtract(begTime).TotalSeconds * 0.8))
+                .ToList();
+            
+            if(moreThan80PersentIntersectDialogues.Count==0)
+                return false;
+            else
+                return true;
+        }
+        
         private List<MarkUp> UpdateMarkUp(MarkUp markUp, ElasticClient log, double persent = 0.7)
         {
             var updatedMarkUp = new List<MarkUp>();
