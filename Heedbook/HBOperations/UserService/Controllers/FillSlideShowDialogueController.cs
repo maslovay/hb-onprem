e24b2ba2bd8b197ee849404990a036241348834a
annushka34 <annsamoluk82@gmail.com>
2020-03-13 16:15:13 +0200

    test

--- Heedbook/HBOperations/UserService/Controllers/FillSlideShowDialogueController.cs
+++ Heedbook/HBOperations/UserService/Controllers/FillSlideShowDialogueController.cs
@@ -1,9 +1,13 @@
-﻿using HBLib.Utils;
+﻿using HBData;
+using HBLib.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Notifications.Base;
 using RabbitMqEventBus.Events;
 using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Globalization;
+using System.Linq;
 
 namespace UserService.Controllers
 {
@@ -12,13 +16,16 @@ namespace UserService.Controllers
     [ApiController]
     public class FillSlideShowDialogueController : Controller
     {
+        private readonly RecordsContext _context;
         private readonly INotificationHandler _handler;
         private readonly CheckTokenService _service;
 
-        public FillSlideShowDialogueController(INotificationHandler handler, CheckTokenService service)
+        public FillSlideShowDialogueController(INotificationHandler handler, CheckTokenService service,
+                                                    RecordsContext context)
         {
             _handler = handler;
             _service = service;
+            _context = context;
         }
 
         [HttpPost]
@@ -27,6 +34,26 @@ namespace UserService.Controllers
         {
             if (!_service.CheckIsUserAdmin()) return BadRequest("Requires admin role");
             _handler.EventRaised(message);
+            return Ok();
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Description = "Fill in SlideShowSessions DialogueId")]
+        public IActionResult FillSlideShowDialoguesAll([FromHeader] string begTime)
+        {
+            if (!_service.CheckIsUserAdmin()) return BadRequest("Requires admin role");
+
+            var dateFormat = "HH:mm:ss dd.MM.yyyy";
+            var timeBeg = DateTime.ParseExact(begTime, dateFormat, CultureInfo.InvariantCulture);
+            var dialogues = _context.Dialogues.Where(x => x.BegTime >= timeBeg && x.StatusId == 3).Select(x => x.DialogueId).ToList();
+            foreach (var dId in dialogues)
+            {
+                var @eventFillSlideShowDialogue = new FillSlideShowDialogueRun
+                {
+                    DialogueId = dId
+                };
+                _handler.EventRaised(eventFillSlideShowDialogue);
+            }
             return Ok();
         }
     }
