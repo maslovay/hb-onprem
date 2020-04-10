186f8f77fa07b8399368732fd850dea919d9f2e0
Oleg Pinarin <kertak@yandex.ru>
2020-04-10 16:06:38 +0300

    fixedReturnPropertyFirstLetter as UpperCase

--- Heedbook/HBOperations/HBMLOnlineService/Startup.cs
+++ Heedbook/HBOperations/HBMLOnlineService/Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using UserOperations.Utils;
+using Newtonsoft.Json.Serialization;
 
 namespace HBMLOnlineService
 {
@@ -95,6 +96,8 @@ namespace HBMLOnlineService
             services.AddTransient(provider => provider.GetRequiredService<IOptions<SftpSettings>>().Value);
             services.AddTransient<SftpClient>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
