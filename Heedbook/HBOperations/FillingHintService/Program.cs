8c6da9ce4c1d09ed182650209b8fefd53551795a
Oleg Pinarin <kertak@yandex.ru>
2020-04-13 13:22:00 +0300

    added FillintHintServiceTests

--- Heedbook/HBOperations/FillingHintService/Program.cs
+++ Heedbook/HBOperations/FillingHintService/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using ServiceExtensions;
 
 namespace FillingHintService
 {
@@ -14,7 +15,8 @@ namespace FillingHintService
         public static IWebHostBuilder CreateWebHostBuilder(String[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
-                          .UseStartup<Startup>();
+                .ConfigureBuilderDueToEnvironment(args: args, portToReassignForTests: 5075)
+                .UseStartup<Startup>();
         }
     }
 }
