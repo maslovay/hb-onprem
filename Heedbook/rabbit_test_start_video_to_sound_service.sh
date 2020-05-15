39522555a806d07d80c02fc44af2e745762443f4
kertak90 <35306203+kertak90@users.noreply.github.com>
2020-05-15 19:02:07 +0300

    Update rabbit_test_start_video_to_sound_service.sh
    fix

--- Heedbook/rabbit_test_start_video_to_sound_service.sh
+++ Heedbook/rabbit_test_start_video_to_sound_service.sh
@@ -32,8 +32,6 @@ service --status-all
 mkdir /app/HBOperations/VideoToSoundServiceTests/TestResults/
 cd /app/HBOperations/VideoToSoundServiceTests/
 dotnet test --logger:"trx;LogFileName=results.trx" ; base64 /app/HBOperations/VideoToSoundServiceTests/TestResults/results*.trx > /app/HBOperations/VideoToSoundServiceTests/TestResults/results_base64 ;
-<<<<<<< HEAD
-=======
 if grep -c 'outcome="Failed"' /app/HBOperations/VideoToSoundServiceTests/TestResults/results*.trx
 then
 	echo "exit"
@@ -41,6 +39,5 @@ then
 else
 	echo "Test Pass"
 fi
->>>>>>> origin/devices
 curl -X POST "https://heedbookapi.northeurope.cloudapp.azure.com/user/ExpressTester/PublishUnitTestResults" -H  "accept: application/json" -H  "Content-Type: application/json-patch+json" -d "{ \"TrxTextBase64\" : \"$(cat /app/HBOperations/VideoToSoundServiceTests/TestResults/results_base64)\" }";
 echo test ended
