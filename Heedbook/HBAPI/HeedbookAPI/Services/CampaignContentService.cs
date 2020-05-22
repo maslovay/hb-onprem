13f301c2b209f0802c430c3e9f9e00e303abdd11
Oleg Pinarin <kertak@yandex.ru>
2020-05-22 17:54:44 +0300

    fix

--- Heedbook/HBAPI/HeedbookAPI/Services/CampaignContentService.cs
+++ Heedbook/HBAPI/HeedbookAPI/Services/CampaignContentService.cs
@@ -336,8 +336,9 @@ namespace UserOperations.Services
                 MyClient.Method = WebRequestMethods.Http.Get;
                 // MyClient.UseDefaultCredentials = true;
                 // MyClient.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                MyClient.UserAgent = "[any words that is more than 5 characters]";
                 MyClient.Headers.Add("Accept: text/html, application/xhtml+xml, */*");
-                MyClient.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
+                // MyClient.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
                 MyClient.Headers.Add(HttpRequestHeader.ContentType, "application/json");
                 MyClient.Headers.Add("Accept-Encoding", "gzip, deflate, br");
                 MyClient.Headers.Add("Connection", "keep-alive");
