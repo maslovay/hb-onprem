e78b0e682e79111ccb36476ca9345b831cb9d84a
nkrokhmal <krokhmal11@mail.ru>
2020-06-01 18:29:25 +0300

    Fix

---
+++ Heedbook/HBOperations/STTService/Models/RecognitionResult.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace Models{
+    public class RecognitionResult
+    {
+        public List<WordRecognizedResult> result;
+    }
+}
---
+++ Heedbook/HBOperations/STTService/Models/WordRecognized.cs
@@ -0,0 +1,9 @@
+namespace Models{
+    public class WordRecognized
+    {
+        public double Time { get; set; }
+        
+        public double Duration { get; set; }
+        public string Word { get; set; }
+    }
+}
