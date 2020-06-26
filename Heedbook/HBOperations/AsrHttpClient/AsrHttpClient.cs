fe89978d86d1e931e68ce5820ad230b7420ba6e4
nkrokhmal <krokhmal11@mail.ru>
2020-06-26 14:39:45 +0300

    Fix

--- Heedbook/HBOperations/AsrHttpClient/AsrHttpClient.cs
+++ Heedbook/HBOperations/AsrHttpClient/AsrHttpClient.cs
@@ -21,7 +21,10 @@ namespace AsrHttpClient
                 : _asrSettings.Uri + "/" + AudioRecognize + dialogueId;
             var uri = new Uri(path);
             var client = new HttpClient();
-            await client.GetAsync(uri);
+            System.Console.WriteLine($"Send request to url {uri}");
+            var content = await client.GetAsync(uri);
+            System.Console.WriteLine($"Status code is {content.StatusCode}");
+            System.Console.WriteLine($"Http request content id {content.Content.ReadAsStringAsync()}");
         }
     }
 }
