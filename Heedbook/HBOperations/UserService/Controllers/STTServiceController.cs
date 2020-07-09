441eaf950f50bf3763f4090c503f2a5248763917
nkrokhmal <krokhmal11@mail.ru>
2020-07-09 15:36:29 +0300

    Fix

--- Heedbook/HBOperations/UserService/Controllers/STTServiceController.cs
+++ Heedbook/HBOperations/UserService/Controllers/STTServiceController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using HBLib.Utils;
 using HBMLHttpClient;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+using RabbitMqEventBus;
+using RabbitMqEventBus.Events;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Notifications.Base;
@@ -21,22 +21,38 @@ namespace UserService.Controllers
     {
         private readonly INotificationHandler _handler;
         private readonly HbMlHttpClient _client;
+        private readonly INotificationPublisher _publisher;
         private readonly CheckTokenService _service;
 
-        public STTController(INotificationHandler handler, HbMlHttpClient client, CheckTokenService service)
+        public STTController(INotificationHandler handler, HbMlHttpClient client, CheckTokenService service, INotificationPublisher publisher)
         {
             _handler = handler;
             _client = client ?? throw new ArgumentNullException(nameof(client));
             _service = service;
+            _publisher = publisher;
         }
 
         [HttpPost]
         [SwaggerOperation(Description =
             "Run local stt for dialogue")]
-        public async Task<IActionResult> FaceAnalyzeRun([FromBody] STTMessageRun message)
+        public async Task<IActionResult> STTRun1([FromBody] STTMessageRun message)
         {
           //  if (!_service.CheckIsUserAdmin()) return BadRequest("Requires admin role");
+            System.Console.WriteLine($"Sending message {JsonConvert.SerializeObject(message)}");
             _handler.EventRaised(message);
+            System.Console.WriteLine("Sended");
+            return Ok();
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Description =
+            "Run local stt for dialogue second variant")]
+        public async Task<IActionResult> STTRun2([FromBody] STTMessageRun message)
+        {
+          //  if (!_service.CheckIsUserAdmin()) return BadRequest("Requires admin role");
+            System.Console.WriteLine($"Sending message {JsonConvert.SerializeObject(message)}");
+            _publisher.Publish(message);
+            System.Console.WriteLine("Sended");
             return Ok();
         }
 
