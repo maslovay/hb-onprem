a1174d75c16f27c444d3022fe7544b56c3453cd1
nkrokhmal <krokhmal11@mail.ru>
2020-07-14 10:40:52 +0300

    Fix

---
+++ Heedbook/HBOperations/TestServer/Startup.cs
@@ -0,0 +1,54 @@
+﻿ 
+using HBData;
+using HBLib;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Notifications.Base;
+using Quartz;
+using HBLib.Utils;
+using Configurations;
+
+namespace TestServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {   
+            services.AddOptions();
+
+            services.AddRabbitMqEventBus(Configuration);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<DescriptorCalculations>();
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var service = app.ApplicationServices.GetRequiredService<INotificationService>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
