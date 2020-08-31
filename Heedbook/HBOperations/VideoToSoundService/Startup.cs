798f315d8fd40ba0a9789b873a094af87e72786c
nkrokhmal <krokhmal11@mail.ru>
2020-08-31 09:37:17 +0300

    Fix

--- Heedbook/HBOperations/VideoToSoundService/Startup.cs
+++ Heedbook/HBOperations/VideoToSoundService/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using Quartz;
 using QuartzExtensions;
 using RabbitMqEventBus;
@@ -28,25 +29,37 @@ namespace VideoToSoundService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOptions();
+            System.Console.WriteLine("Configure video to sound services");
             services.AddTransient<VideoToSound>();
             services.AddTransient<VideoToSoundRunHandler>();
+            
+            System.Console.WriteLine("Configure sftp client");
             services.AddTransient<SftpClient>();
-            services.Configure<FFMpegSettings>(Configuration.GetSection(nameof(FFMpegSettings)));
             services.Configure<SftpSettings>(Configuration.GetSection(nameof(SftpSettings)));
+            services.AddTransient(provider => provider.GetRequiredService<IOptions<SftpSettings>>().Value);
+
+            System.Console.WriteLine("Configure ffmpeg");
+            services.Configure<FFMpegSettings>(Configuration.GetSection(nameof(FFMpegSettings)));
             services.AddTransient(provider => provider.GetRequiredService<IOptions<FFMpegSettings>>().Value);
+
+            System.Console.WriteLine("Configure elastic");
             services.Configure<ElasticSettings>(Configuration.GetSection(nameof(ElasticSettings)));
             services.AddScoped(provider => provider.GetRequiredService<IOptions<ElasticSettings>>().Value);
             services.AddScoped<ElasticClientFactory>();
             services.AddTransient<FFMpegWrapper>();
-            services.AddTransient(provider => provider.GetRequiredService<IOptions<SftpSettings>>().Value);
+            
+            System.Console.WriteLine("Configure rabbitmq");
             services.AddRabbitMqEventBus(Configuration);
             services.AddDeleteOldFilesQuartz();
+
+            System.Console.WriteLine("Inited all services");
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IScheduler scheduler)
         {
+            System.Console.WriteLine("Subscribe");
             var publisher = app.ApplicationServices.GetRequiredService<INotificationPublisher>();
             publisher.Subscribe<VideoToSoundRun, VideoToSoundRunHandler>();
             if (env.IsDevelopment())
@@ -59,6 +72,7 @@ namespace VideoToSoundService
                 trigger);
             app.UseHttpsRedirection();
             app.UseMvc();
+            System.Console.WriteLine("Started video to sound");
         }
     }
 }
