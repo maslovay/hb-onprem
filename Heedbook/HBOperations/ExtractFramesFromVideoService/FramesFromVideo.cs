d66b156a700ae7fe26828e1ded4a69a908be0de5
Oleg Pinarin <kertak@yandex.ru>
2020-11-12 12:23:12 +0300

    fix FileFrame model

--- Heedbook/HBOperations/ExtractFramesFromVideoService/FramesFromVideo.cs
+++ Heedbook/HBOperations/ExtractFramesFromVideoService/FramesFromVideo.cs
@@ -283,8 +283,7 @@ namespace ExtractFramesFromVideo
                 IsFacePresent = false,
                 StatusId = 6,
                 StatusNNId = 6,
-                Time = DateTime.ParseExact(frameTime, "yyyyMMddHHmmss", CultureInfo.InvariantCulture),
-                CreationTime = DateTime.UtcNow
+                Time = DateTime.ParseExact(frameTime, "yyyyMMddHHmmss", CultureInfo.InvariantCulture)
             };
             return fileFrame;
         }
